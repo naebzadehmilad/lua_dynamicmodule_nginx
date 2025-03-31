055e1577d00c4d1c3fa4b8c5dd63ab4fa1020420
Milad.Naebzadeh <45167308+naebzadehmilad@users.noreply.github.com>
2025-04-01 02:36:20 +0330

    Update 0-before.sh

--- 0-before.sh
+++ 0-before.sh
@@ -1,1 +1,6 @@
-apt -y install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev git zip unzip
+apt -y install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev git zip unzip libluajit-5.1-dev luarocks || handle_error "Failed to install packages."
+luarocks install lua-resty-core || handle_error "Failed to install lua-resty-core."
+luarocks install lua-resty-http || handle_error "Failed to install lua-resty-http."
+echo "Setting up PCRE symlink..."
+ ln -sf /usr/lib/x86_64-linux-gnu/libpcre.so.1 /usr/lib/libpcre.so.1 || handle_error "Failed to create PCRE symlink."
+ldconfig || handle_error "Failed to update library cache."
